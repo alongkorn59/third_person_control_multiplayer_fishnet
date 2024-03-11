8a72dd0ba328a037ba4a18e16f8f2ef6e6974e6c
alongkorns <alongkorn59@gmail.com>
2024-03-11 21:03:17 +0700

    [Add] Import model

---
+++ TPS Control (Multiplayer Fishnet)/Assets/Mobile/Scripts/CanvasInputs/UICanvasControllerInput.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class UICanvasControllerInput : MonoBehaviour
+{
+
+    [Header("Output")]
+    public PlayerInputs playerInputs;
+
+    public void VirtualMoveInput(Vector2 virtualMoveDirection)
+    {
+        playerInputs.MoveInput(virtualMoveDirection);
+    }
+
+    public void VirtualLookInput(Vector2 virtualLookDirection)
+    {
+        playerInputs.LookInput(virtualLookDirection);
+    }
+
+    public void VirtualJumpInput(bool virtualJumpState)
+    {
+        playerInputs.JumpInput(virtualJumpState);
+    }
+
+    public void VirtualSprintInput(bool virtualSprintState)
+    {
+        playerInputs.SprintInput(virtualSprintState);
+    }
+
+    public void VirtualCrouchInput(bool virtualCrouchState)
+    {
+        playerInputs.CrouchInput(virtualCrouchState);
+    }
+
+    public void VirtualProneInput(bool virtualProneState)
+    {
+        playerInputs.ProneInput(virtualProneState);
+    }
+
+}
